14a9e01b5ff69908961d384059fae7264cc4ccb9
ejbarrosgnecco <ejbarrosgnecco@yahoo.com>
2023-09-22 12:19:01 +0100

    Amend file structure for missing module

--- server/account/views.py
+++ server/account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
+from account.controllers.login import account_login
 
 # Create your views here.
-def home(request):
+def login(request):
     return HttpResponse("Hello, world!")
